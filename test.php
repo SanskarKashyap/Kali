938e9365045f578379007171264d61912c73b287
SanskarKashyap <sanskarkashyap308@gmail.com>
2023-08-14 16:37:13 +0530

    CTF

---
+++ test.php
@@ -0,0 +1,11 @@
+//SUM of 2 number 
+// 1. get the 2 number from the user
+// 2. add the 2 number
+// 3. display the sum of 2 number
+
+<?php
+var a = 10;
+var b = 20;
+var c = a+b;
+echo "sum of 2 number is : " + c;
+?>
